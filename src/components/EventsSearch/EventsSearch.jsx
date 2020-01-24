de2e760afb48603ee752b5932bb9ff05117f0abd
Grant <gmontgomery1994@gmail.com>
2020-01-23 22:45:35 -0800

    started moving event search to something separate.

---
+++ src/components/EventsSearch/EventsSearch.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import css from "./EventsSearch.css";
+import { useState } from "react";
+const EventsSearch = () => {
+  let [category, changeCategory] = useState("");
+  return (
+    <div className={`eventsSearchWrapper ${css.eventsSearchWrapper}`}>
+      <div className={`eventsSearchTextWrapper ${css.eventsSearchTextWrapper}`}>
+        <p>What type of events are you looking for?</p>
+      </div>
+      <select
+        name=""
+        id=""
+        value={category}
+        onChange={event => changeCategory((category = event.target.value))}
+        className={`eventCategories ${css.eventsCategories}`}
+      >
+        <option value="">Select a category (Optional)</option>
+        <option value="All">All</option>
+        <option value="Music">Music</option>
+        <option value="Nightlife">Nightlife</option>
+        <option value="Food & Drink">Food & Drink</option>
+        <option value="Film">Film</option>
+        <option value="Sports & Active Lifestyle">
+          Sports & Active Lifestyle
+        </option>
+        <option value="Festivals & Fairs">Festivals & Fairs</option>
+        <option value="Arts & Theatre">Arts & Theatre</option>
+        <option value="Other">Other</option>
+      </select>
+    </div>
+  );
+};
+
+export default EventsSearch;
