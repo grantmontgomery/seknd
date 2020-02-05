1f0a5d2c9b3709ae56478fc5aa99c7cd52346b3d
Grant <gmontgomery1994@gmail.com>
2020-02-05 12:56:40 -0800

    more search result set up.

--- src/components/Search-Results-Bar-Label/SearchResultsBarLabel.jsx
+++ src/components/Search-Results-Bar-Label/SearchResultsBarLabel.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import css from "./SearchResultsBarLabel.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const SearchResultsBarLabel = () => {
+const SearchResultsBarLabel = props => {
+  const [state, setState] = useState({ class: "", name: "" });
+
+  const { type } = props;
+
+  const setType = type => {
+    if (type === "events") {
+      setState({ class: `eventsClass ${css.eventsClass}`, name: "Events" });
+    } else {
+      setState({ class: `placesClass ${css.placesClass}`, name: "Places" });
+    }
+  };
+
+  useEffect(() => {
+    setType(type);
+  }, [props]);
+
   return (
     <div
-      className={`searchResultsBarLabelWrapper ${css.searchResultsBarLabelWrapper}`}
+      className={`searchResultsBarLabelWrapper ${css.searchResultsBarLabelWrapper} ${state.class}`}
     >
-      <div className={`labelTextWrapper ${css.labelTextWrapper}`}>Events</div>
+      <div className={`labelTextWrapper ${css.labelTextWrapper}`}>
+        {state.name}
+      </div>
     </div>
   );
 };
--- src/components/Search-Results-Bar/SearchResultsBar.jsx
+++ src/components/Search-Results-Bar/SearchResultsBar.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import css from "./SearchResultsBar.css";
+import { useEffect, useState } from "react";
 
-const SearchResultsBar = () => {
+const SearchResultsBar = props => {
+  const [state, setState] = useState({ class: "" });
+
+  const { type } = props;
+
+  const setType = type => {
+    if (type === "events") {
+      setState({ class: `eventsClass ${css.eventsClass}` });
+    } else {
+      setState({ class: `placesCall ${css.placesCall}` });
+    }
+  };
+  useEffect(() => {
+    setType(type);
+  }, [props]);
   return (
-    <div
-      className={`searchResultsBarWrapper ${css.searchResultsBarWrapper}`}
-    ></div>
+    <div className={`searchResultsBarWrapper ${css.searchResultsBarWrapper}`}>
+      <button className={`slideButton ${css.slideButton} ${state.class}`}>
+        {"<"}
+      </button>
+      <button className={`slideButton ${css.slideButton} ${state.class}`}>
+        {">"}
+      </button>
+    </div>
   );
 };
 
