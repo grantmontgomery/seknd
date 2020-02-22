82f58f2065ab8fe1bdc1127c116178ae21f6a342
Grant <gmontgomery1994@gmail.com>
2020-02-22 11:56:21 -0800

    minor changes. Making the date parts component a reusable component without shape changing.

--- src/components/Date-Parts/DateParts.jsx
+++ src/components/Date-Parts/DateParts.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { DatePartsPiece } from "../Date-Parts-Piece";
-import { ExitButton, NewInput, ColorSelector } from "./Parts";
+import { NewInput, ColorSelector } from "./Parts";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import css from "./DateParts.css";
 
 const DateParts = () => {
-  const [state, setState] = useState({ shape: "partsCircle" });
+  // const [state, setState] = useState({ shape: "partsCircle" });
 
-  const { shape } = state;
+  // const { shape } = state;
 
   const dateParts = useSelector(state => state.datePartsReducer);
 
-  const changeShape = event => {
-    event.preventDefault();
-    const { target } = event;
-    const button = target.className;
-    console.log(button);
-    if (shape === "partsCircle") {
-      setState({ shape: "partsExtended" });
-    } else if (
-      shape === "partsExtended" &&
-      button.includes("partsExtendedExit") === true
-    ) {
-      setState({ shape: "partsCircle" });
-    }
-  };
+  // const changeShape = event => {
+  //   event.preventDefault();
+  //   const { target } = event;
+  //   const button = target.className;
+  //   if (shape === "partsCircle") {
+  //     setState({ shape: "partsExtended" });
+  //   } else if (
+  //     shape === "partsExtended" &&
+  //     button.includes("partsExtendedExit") === true
+  //   ) {
+  //     setState({ shape: "partsCircle" });
+  //   }
+  // };
 
   const applyTransitions = () => {
     return dateParts.map(part => (
@@ -35,29 +34,32 @@ const DateParts = () => {
     ));
   };
 
-  const changeInternals = () => {
-    if (shape === "partsExtended") {
-      return (
-        <React.Fragment>
-          <ExitButton></ExitButton>
-          <div className={`partsHeader ${css.partsHeader}`}>Date Parts</div>
-          <NewInput></NewInput>
-          <div className={`piecesWrapper ${css.piecesWrapper}`}>
-            <TransitionGroup>{applyTransitions()}</TransitionGroup>
-          </div>
-        </React.Fragment>
-      );
-    }
-  };
+  // const changeInternals = () => {
+  //   if (shape === "partsExtended") {
+  //     return (
+  //       <React.Fragment>
+  //         <ExitButton></ExitButton>
+  //         <div className={`partsHeader ${css.partsHeader}`}>Date Parts</div>
+  //         <NewInput></NewInput>
+  //         <div className={`piecesWrapper ${css.piecesWrapper}`}>
+  //           <TransitionGroup>{applyTransitions()}</TransitionGroup>
+  //         </div>
+  //       </React.Fragment>
+  //     );
+  //   }
+  // };
 
   return (
     <div
-      className={`datePartsWrapper ${css.datePartsWrapper} ${shape} ${
-        css[`${shape}`]
-      }`}
-      onClick={changeShape}
+      className={`datePartsWrapper ${css.datePartsWrapper} 
+     `}
+      // onClick={changeShape}
     >
-      {changeInternals()}
+      <div className={`partsHeader ${css.partsHeader}`}>Date Parts</div>
+      <NewInput></NewInput>
+      <div className={`piecesWrapper ${css.piecesWrapper}`}>
+        <TransitionGroup>{applyTransitions()}</TransitionGroup>
+      </div>
     </div>
   );
 };
