195d53998e1e9f8f1dd51a7401f7214b8035dc3e
Grant <gmontgomery1994@gmail.com>
2020-03-06 11:58:40 -0800

    more grid styling work based on time difference

--- src/components/Scheduler-Grid/SchedulerGrid.jsx
+++ src/components/Scheduler-Grid/SchedulerGrid.jsx
@@ -1,108 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { SchedulerGridSquare } from "../Scheduler-Grid-Square";
 import { useSelector } from "react-redux";
 import css from "./SchedulerGrid.css";
+import { useEffect } from "react";
 
-const SchedulerGrid = props => {
+const SchedulerGrid = ({ timeDifference }) => {
+  const [style, setStyle] = useState({
+    width: "",
+    gridTemplateColumns: "",
+    gridTemplateAreas: ""
+  });
+
+  const [squares, setSquares] = useState([]);
+
   const repeatString = (string, number) => {
     let newString = "";
     while (number > 0) {
       newString += string;
       number--;
     }
+    return newString;
   };
 
-  const gridSliderStyle = () => {
-    const { hourDifference } = props;
-    const width = (hourDifference + 1) * 200;
-    const amountOfColumns = width / 100;
-    const gridColumnSize = " 100px";
-    const squareStr = " square";
-    const headerStr = " header";
-    const rowStr = ` "${repeatString(squareStr, amountOfColumns)}"`;
-    const firstRow = `"${repeatString(headerStr, amountOfColumns)}"`;
-    return {
-      width: `${width}px`,
-      gridTemplateColumns: ` ${repeatString(gridColumnSize, amountOfColumns)}`,
-      gridTemplateAreas: `${firstRow}${repeatString(rowStr, 7)}`
-    };
-  };
+  useEffect(() => {
+    setGridStyle(timeDifference);
+  }, []);
 
-  const renderHoursSections = () => {
-    let { startHour, startDay, endDay, endHour } = props;
-    if (startDay !== endDay) {
-      const hours = [];
-      while (startHour < 24) {
-        if (startHour > 12) {
-          hours.push(`${startHour - 12}:00 p.m`);
-          startHour++;
-        } else if (startHour === 0) {
-          hours.push(`12:00 a.m`);
-          startHour++;
-        } else if (startHour === 12) {
-          hours.push(`12:00 p.m`);
-        } else {
-          hours.push(`${startHour}:00 a.m`);
-          startHour++;
-        }
-      }
-      let endHourCount = 0;
-      while (endHourCount <= endHour) {
-        if (endHourCount > 12) {
-          hours.push(`${endHourCount - 12}:00 p.m`);
-          endHourCount++;
-        } else if (endHourCount === 0) {
-          hours.push(`12:00 a.m`);
-          endHourCount++;
-        } else if (endHourCount === 12) {
-          hours.push(`12:00 p.m`);
-          endHourCount++;
-        } else {
-          hours.push(`${endHourCount}:00 a.m`);
-          endHourCount++;
-        }
-      }
-
-      return hours.map(hour => {
-        return (
-          <div className="hours-wrapper">
-            <p>{hour}</p>
-          </div>
-        );
-      });
-    } else {
-      const hours = [];
-      while (startHour <= endHour) {
-        if (startHour > 12) {
-          hours.push(`${startHour - 12}:00 p.m`);
-          startHour++;
-        } else if (startHour === 0) {
-          hours.push(`12:00 a.m`);
-          startHour++;
-        } else if (startHour === 12) {
-          hours.push(`12:00 p.m`);
-        } else {
-          hours.push(`${startHour}:00 a.m`);
-          startHour++;
-        }
-      }
-      return hours.map(hour => {
-        return (
-          <div className="hours-wrapper" style={{ width: "200px" }}>
-            <p>{hour}</p>
-          </div>
-        );
-      });
-    }
+  const setGridStyle = () => {
+    let width = (timeDifference + 1) * 200;
+    let gridColumnSize = " 100px";
+    let amountOfColumns = width / 100;
+    let rowString = ` "${repeatString(" square", amountOfColumns)}`;
+    let firstRow = `${repeatString(" header", amountOfColumns)}`;
+    let gridTemplateAreas = `${(firstRow, repeatString(rowString, 5))}`;
+    let gridTemplateColumns = repeatString(gridColumnSize, amountOfColumns);
+    setStyle({ gridTemplateColumns, gridTemplateAreas, width: `${width}px` });
   };
 
   return (
     <div className={`schedulerGridWrapper ${css.schedulerGridWrapper}`}>
-      <div className={`gridSlider ${css.gridSlider}`}>
+      <div className={`gridSlider ${css.gridSlider}`} {...style}>
         <div className={`gridDateHeader ${css.gridDateHeader}`}></div>
-        {/* {props.squares.map(square => (
-          <SchedulerGridSquare key={square}></SchedulerGridSquare>
-        ))} */}
         <SchedulerGridSquare></SchedulerGridSquare>
       </div>
     </div>
