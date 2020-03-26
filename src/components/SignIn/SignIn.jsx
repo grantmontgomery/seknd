9b914f6191f99e2b8397bccc247323a1083cc273
Grant <gmontgomery1994@gmail.com>
2020-03-26 14:00:10 -0700

    working with intersectionObserver.

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import css from "./SignIn.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { loadOptions } from "@babel/core";
 
 const SignIn = () => {
-  return <div className={`signInWrapper ${css.signInWrapper}`}></div>;
+  let [element, setElement] = useState(null);
+  const square = useRef();
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentElement.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
+  const observer = useRef(
+    new IntersectionObserver(
+      entries => {
+        const first = entries[0];
+
+        console.log(first);
+      },
+      {
+        threshold: 1.0
+      }
+    )
+  );
+
+  return (
+    <div className={`signInWrapper ${css.signInWrapper}`}>
+      <div className={`testSquare ${css.testSquare}`} ref={setElement}>
+        <div
+          className={`secondSquare ${css.secondSquare}`}
+          onClick={() => console.log(square.current)}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
