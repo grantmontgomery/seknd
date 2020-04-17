0e8c2775f41ca543677e9bb8a47cd9e762c7dc22
Grant <gmontgomery1994@gmail.com>
2020-04-16 21:55:37 -0700

    some small changes before actually working on changeWidthFeature2

--- src/components/Date-Parts-Piece/Parts/DragPiece/DragParts/LengthenPart/LengthenPart.js
+++ src/components/Date-Parts-Piece/Parts/DragPiece/DragParts/LengthenPart/LengthenPart.js
@@ -2,7 +2,6 @@ import React from "react";
 import css from "./LengthenPart.css";
 
 const LengthenPart = ({ rotateArrow }) => {
-  console.log(rotateArrow);
   return (
     <div className={`lengthenWrapper ${css.lengthenWrapper}`}>
       <div className={`oWrapper ${css.oWrapper}`}>O</div>
