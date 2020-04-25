da630197232ae5e8c5c8b39b235e6eee0e9549de
Grant <gmontgomery1994@gmail.com>
2020-04-24 19:24:10 -0700

    working on equation.

--- src/components/Date-Parts-Piece/Logic/endTime.js
+++ src/components/Date-Parts-Piece/Logic/endTime.js
@@ -1,7 +1,28 @@
 const endTime = (startTime, partLength) => {
-  const number = parseInt(startTime.substring(0, startTime.search(":")));
+  const hour = parseInt(startTime.substring(0, startTime.search(":")));
 
-  retirm;
+  const minutes = parseInt(
+    startTime.substring(startTime.search(":") + 1, startTime.search(" "))
+  );
+
+  const amPm = startTime.substring(startTime.search(" ") + 1);
+
+  const hourAdded = (num) => (num += hour > 12 ? num + hour - 12 : num + hour);
+
+  const minutesAdded = (num) => {
+    if (num === 30) {
+      if (minutes === 30) {
+        return "00";
+      } else {
+        return "30";
+      }
+    } else {
+      return `${minutes}`;
+    }
+  };
+
+  if (partLength === "1 hour") {
+  }
 };
 
 export default endTime;
--- src/components/Date-Parts-Piece/Logic/index.js
+++ src/components/Date-Parts-Piece/Logic/index.js
@@ -1,3 +1,3 @@
 export { default as Logic } from "./Logic";
 export { default as limitPartTitle } from "./limitPartTitle";
-export { default as endTime } from "./endTime";
+// export { default as endTime } from "./endTime";
