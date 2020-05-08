614cb9ae0ce1b667148f9fe1cba745fdb74636f6
Grant <gmontgomery1994@gmail.com>
2020-05-08 12:45:56 -0700

    hamburger made.

--- src/components/Nav/Parts/MobileNav/MobileNav.jsx
+++ src/components/Nav/Parts/MobileNav/MobileNav.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import css from "../../Nav.css";
+import { useState } from "react";
 
 const MobileNav = () => {
-  return <div className={`mobileWrapper ${css.mobileWrapper}`}></div>;
+  const [clicked, transform] = useState(false);
+
+  const shape = () => (clicked ? "exit" : "hamburger");
+
+  return (
+    <div
+      className={`mobileWrapper ${css.mobileWrapper} ${shape()} ${
+        css[`${shape()}`]
+      }`}
+      onClick={() => (clicked ? transform(false) : transform(true))}
+    >
+      <div className={`topLine ${css.topLine}`}></div>
+      <div className={`middleLine ${css.middleLine}`}></div>
+      <div className={`bottomLine ${css.bottomLine}`}></div>
+    </div>
+  );
 };
 
 export default MobileNav;
