91a8205354fecce70062db2942dc3d890fefdebe
Grant <gmontgomery1994@gmail.com>
2020-05-08 23:52:51 -0700

    nav set up with positioning.

--- src/components/Nav/Parts/index.js
+++ src/components/Nav/Parts/index.js
@@ -1,3 +1,4 @@
 export { MobileNav } from "./MobileNav";
 export { LinksWrapper } from "./LinksWrapper";
 export { MobileLinks } from "./MobileLinks";
+export { DatePartsMobile } from "./DatePartsMobile";
