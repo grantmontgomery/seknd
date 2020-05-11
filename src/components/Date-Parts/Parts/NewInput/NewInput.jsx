8a495a2502f60d7be220c73be4d63a48d1b30ec8
Grant <gmontgomery1994@gmail.com>
2020-05-10 19:11:43 -0700

    small styling edits for date parts section.

--- src/components/Date-Parts/Parts/NewInput/NewInput.jsx
+++ src/components/Date-Parts/Parts/NewInput/NewInput.jsx
@@ -3,13 +3,21 @@ import css from "./NewInput.css";
 
 const NewInput = ({ handleChange, name, location }) => {
   return (
-    <div className={`newInputWrapper ${css.newInputWrapper}`}>
+    <div
+      className={`newInputWrapper ${css.newInputWrapper} ${location} ${
+        css[`${location}`]
+      }`}
+    >
       <input
         type="text"
         value={name}
         input="name"
         onChange={handleChange}
-        placeholder="Another idea? Type it here."
+        placeholder={`${
+          location === "navLinks"
+            ? "Type out custom ideas."
+            : "Another idea? Type it here?"
+        }`}
       />
     </div>
   );
