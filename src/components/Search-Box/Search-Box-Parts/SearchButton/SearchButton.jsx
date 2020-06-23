d9db474a45e62d89d58232eb8077b34f629bbbbc
Grant <gmontgomery1994@gmail.com>
2020-06-23 11:41:54 -0700

    search button with link changed.

--- src/components/Search-Box/Search-Box-Parts/SearchButton/SearchButton.jsx
+++ src/components/Search-Box/Search-Box-Parts/SearchButton/SearchButton.jsx
@@ -39,35 +39,9 @@ const SearchButton = ({ query, searchType, componentLocation }) => {
     return homeToSearch(searchType, query) === true ? linked() : nonLinked();
   };
 
-  return componentLocation === "home" || componentLocation === "navBar"
+  return componentLocation === "homePage" || componentLocation === "navBar"
     ? linkToPage()
     : nonLinked();
-
-  // return (
-  //   <div
-  //     className={`submitButton ${css.submitButton}`}
-  //     onClick={(event) => handleSubmit(event, query, dispatch, searchType)}
-  //   >
-  //     <div className={`submitTitleWrapper ${css.submitTitleWrapper}`}>
-  //       <span className={`submit ${css.submit}`}>SEARCH</span>
-  //     </div>
-
-  //     <div className={`arrowWrapper ${css.arrowWrapper}`}>
-  //       <svg
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         viewBox="0 0 31.3 55.54"
-  //         className={`arrow ${css.arrow}`}
-  //       >
-  //         <title>Asset 6</title>
-  //         <g id="Layer_2" data-name="Layer 2">
-  //           <g id="Tracing">
-  //             <polygon points="3.54 55.53 0 52 24.23 27.77 0 3.54 3.54 0 31.3 27.77 3.54 55.53" />
-  //           </g>
-  //         </g>
-  //       </svg>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default SearchButton;
